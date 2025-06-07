0ad6b7d893f3c02a70472f41b18e55bf174f0419
Moritz Scheckenbach <moritz.scheckenbach@gmail.com>
2025-06-07 15:47:58 +0200

    neuer image capture code

--- packages/image_capture/src/image_capture.py
+++ packages/image_capture/src/image_capture.py
@@ -1,12 +1,10 @@
-import os
-
 import cv2
 import numpy as np
 import rclpy
 from cv_bridge import CvBridge
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from std_msgs.msg import String  # saving directory
+from std_msgs.msg import Bool
 
 
 class ImageCaptureNode(Node):
@@ -16,11 +14,10 @@ class ImageCaptureNode(Node):
         self.bridge = CvBridge()
 
         # Subscriber für Bilder
-        self.image_subscription = self.create_subscription(
-            Image, "/camera/camera/color/image_raw", self.image_callback, 10)
+        self.image_subscription = self.create_subscription(Image, "/camera/camera/color/image_raw", self.image_callback, 10)
 
         # Subscriber für den Trigger
-        self.trigger_subscription = self.create_subscription(String, "/capture_trigger", self.trigger_callback, 10)
+        self.trigger_subscription = self.create_subscription(Bool, "/capture_trigger", self.trigger_callback, 10)
 
         self.image_msg = None
         self.image_counter = 0
@@ -41,16 +38,14 @@ class ImageCaptureNode(Node):
             cv_image = self.bridge.imgmsg_to_cv2(self.image_msg, desired_encoding="bgr8")
         except Exception as e:
             self.get_logger().info(f"Fehler bei der Bildkonvertierung, bitte versuchen Sie es erneut")
-
-        save_path = msg.data  # image directory from trigger
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        # save_path = (f"extrinsic_camera_calibration/extrinsic_camera_calibration/calibration_images/captured_image_{self.image_counter + 1:03}.jpg")
+        save_path = f"src/extrinsic_camera_calibration/extrinsic_camera_calibration/calibration_data/images/{self.image_counter + 1:03}.jpg"
         save_complete = cv2.imwrite(save_path, cv_image)
         if save_complete:
             self.get_logger().info(f"Bild gespeichert: {save_path}")
             self.image_counter += 1
         else:
-            self.get_logger().info(
-                f"Fehler beim Speichern des Bildes {self.image_counter +1}!\nAufnahme wird erneut gestartet.")
+            self.get_logger().info(f"Fehler beim Speichern des Bildes {self.image_counter +1}!")
 
 
 def main(args=None):
